dbe12b46a675fefdce2cc2125c7becc464730996
Dhruv Dhaduk <dhruv.dhaduk@simformsolutions.com>
2025-03-31 14:27:52 +0530

    feature: Cleanup the index.ts and pass selector for add user form in View

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,28 +1,12 @@
-import User from './user';
 import View from './view';
 
 // Local Storage Keys
 const LOCAL_USERS_KEY = 'LOCAL_USERS_KEY';
 
-// Add a local user inside localStorage for testing purpose. (Temporary)
-localStorage.setItem(
-    LOCAL_USERS_KEY,
-    JSON.stringify([
-        new User(
-            'localId1',
-            'Local',
-            'User',
-            25,
-            'local@user.com',
-            '0123456789',
-            'male'
-        ),
-    ])
-);
-
 // Initialize the view to start fetching and rendering the data.
 const viewConfig = {
     usersListId: 'users-list',
+    addUserFormId: 'add-user-form',
 };
 
 new View(LOCAL_USERS_KEY, viewConfig);
